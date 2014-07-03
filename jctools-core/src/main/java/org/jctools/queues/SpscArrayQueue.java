9315ceb5d27c7323a7adaf02602c24d90c523ffc
nitsanw <nitsanw@yahoo.com>
2014-07-03 17:38:10 +0200

    Make OFFER_BATCH_SIZE a field and determine by capacity and bound by MAX to give better ergonomics and allow smaller queues.

--- jctools-core/src/main/java/org/jctools/queues/SpscArrayQueue.java
+++ jctools-core/src/main/java/org/jctools/queues/SpscArrayQueue.java
@@ -15,8 +15,15 @@ package org.jctools.queues;
 
 import static org.jctools.util.UnsafeAccess.UNSAFE;
 
-
-abstract class SpscArrayQueueL1Pad<E> extends ConcurrentCircularArrayQueue<E> {
+abstract class SpscArrayQueueColdField<E> extends ConcurrentCircularArrayQueue<E> {
+    private static final Integer MAX_LOOK_AHEAD_STEP = Integer.getInteger("jctoolts.spsc.max.lookahead.step", 4096);
+    protected final int lookAheadStep;
+    public SpscArrayQueueColdField(int capacity) {
+        super(capacity);
+        lookAheadStep = Math.min(capacity/4, MAX_LOOK_AHEAD_STEP);
+    }
+}
+abstract class SpscArrayQueueL1Pad<E> extends SpscArrayQueueColdField<E> {
     long p10, p11, p12, p13, p14, p15, p16;
     long p30, p31, p32, p33, p34, p35, p36, p37;
 
@@ -88,6 +95,9 @@ abstract class SpscArrayQueueL3Pad<E> extends SpscArrayQueueConsumerField<E> {
  * <a href="http://sourceforge.net/projects/mc-fastflow/">Fast Flow</a> algorithm with an optimization of the offer
  * method taken from the <a href="http://staff.ustc.edu.cn/~bhua/publications/IJPP_draft.pdf">BQueue</a> algorithm (a
  * variation on Fast Flow).<br>
+ * For convenience the relevant papers are available in the resources folder:</br>
+ * <i>2010 - Pisa - SPSC Queues on Shared Cache Multi-Core Systems.pdf</br>
+ * 2012 - Junchang- BQueue- Efﬁcient and Practical Queuing.pdf </br></i>
  * This implementation is wait free.
  * 
  * @author nitsanw
@@ -95,10 +105,9 @@ abstract class SpscArrayQueueL3Pad<E> extends SpscArrayQueueConsumerField<E> {
  * @param <E>
  */
 public final class SpscArrayQueue<E> extends SpscArrayQueueL3Pad<E> {
-    protected static final int OFFER_BATCH_SIZE = Integer.getInteger("offer.batch.size", 4096);
 
     public SpscArrayQueue(final int capacity) {
-        super(Math.max(capacity, 2 * OFFER_BATCH_SIZE));
+        super(capacity);
     }
 
     /**
@@ -114,14 +123,14 @@ public final class SpscArrayQueue<E> extends SpscArrayQueueL3Pad<E> {
         // local load of field to avoid repeated loads after volatile reads
         final E[] lElementBuffer = buffer;
         if (producerIndex >= producerLookAhead) {
-            if (null != lvElement(lElementBuffer, calcElementOffset(producerIndex + OFFER_BATCH_SIZE))) {// LoadLoad
+            if (null != lvElement(lElementBuffer, calcElementOffset(producerIndex + lookAheadStep))) {// LoadLoad
                 return false;
             }
-            producerLookAhead = producerIndex + OFFER_BATCH_SIZE;
+            producerLookAhead = producerIndex + lookAheadStep;
         }
-        soElement(lElementBuffer, calcElementOffset(producerIndex), e);// StoreStore
-        producerIndex++;
-
+        long offset = calcElementOffset(producerIndex);
+        producerIndex++; // do increment here so the ordered store give both a barrier 
+        soElement(lElementBuffer, offset, e);// StoreStore
         return true;
     }
     
@@ -139,8 +148,8 @@ public final class SpscArrayQueue<E> extends SpscArrayQueueL3Pad<E> {
         if (null == e) {
             return null;
         }
+        consumerIndex++; // do increment here so the ordered store give both a barrier
         soElement(lElementBuffer, offset, null);// StoreStore
-        consumerIndex++;
         return e;
     }
 
@@ -156,14 +165,10 @@ public final class SpscArrayQueue<E> extends SpscArrayQueueL3Pad<E> {
 
     @Override
     public int size() {
-        
         int size;
         do {
-            if(peek() == null) {// LoadLoad -> will force load of indexes from memory.
-                return 0;
-            }
             /*
-             * It is possible for a thread to be interrupted or reschedule between the read of the producer
+             * It is possible for the invoking thread to be interrupted or reschedule between the read of the producer
              * and consumer indices, therefore protection is required to ensure size is within valid range.
              */
             final long currentConsumerIndex = lvConsumerIndex();
