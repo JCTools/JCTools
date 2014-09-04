bd424e9c6d686d9e142bb5c32873493443f38010
nitsanw <nitsanw@yahoo.com>
2014-09-04 16:59:58 +0200

    update licence where missing

--- jctools-core/src/main/java/org/jctools/queues/package-info.java
+++ jctools-core/src/main/java/org/jctools/queues/package-info.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /**
  * This package aims to fill a gap in current JDK implementations in offering lock free (wait free where possible)
  * queues for inter-thread message passing with finer grained guarantees and an emphasis on performance.<br>
