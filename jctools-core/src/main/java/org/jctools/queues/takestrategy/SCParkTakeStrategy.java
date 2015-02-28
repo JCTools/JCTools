4feab77e97f0b668407678a181c4caec6ad97a1f
Georges Gomes <georges.gomes@gmail.com>
2015-02-28 16:47:47 +0100

    Thread interrupted check
    Thanks @ChristianWulf

--- jctools-core/src/main/java/org/jctools/queues/takestrategy/SCParkTakeStrategy.java
+++ jctools-core/src/main/java/org/jctools/queues/takestrategy/SCParkTakeStrategy.java
@@ -36,6 +36,10 @@ public final class SCParkTakeStrategy<E> implements TakeStrategy<E>
         while ((e = q.poll()) == null)
         {
             LockSupport.park();
+
+            if (Thread.currentThread().isInterrupted()) {
+                throw new InterruptedException("Interrupted while waiting for the queue to become non-empty.");
+            }
         }
 
         t.lazySet(null);
