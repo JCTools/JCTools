b120af2c3ff2712590374b64cabe788b3025c7e8
nitsanw <nitsanw@yahoo.com>
2015-10-19 10:31:59 +0200

    Reformat

--- jctools-benchmarks/src/main/java/org/jctools/jmh/throughput/MpqThroughputBackoffNone.java
+++ jctools-benchmarks/src/main/java/org/jctools/jmh/throughput/MpqThroughputBackoffNone.java
@@ -39,138 +39,139 @@ import org.openjdk.jmh.infra.Blackhole;
 @Warmup(iterations = 5, time = 3, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 5, time = 3, timeUnit = TimeUnit.SECONDS)
 public class MpqThroughputBackoffNone {
-	private static final long DELAY_PRODUCER = Long.getLong("delay.p", 0L);
-	private static final long DELAY_CONSUMER = Long.getLong("delay.c", 0L);
-	private static final Integer ONE = 777;
-	MessagePassingQueue<Integer> q;
-	private Integer escape;
-	@Param(value = { "SpscArrayQueue", "MpscArrayQueue", "SpmcArrayQueue", "MpmcArrayQueue" })
-	String qType;
-	@Param(value = { "132000" })
-	int qCapacity;
+    private static final long DELAY_PRODUCER = Long.getLong("delay.p", 0L);
+    private static final long DELAY_CONSUMER = Long.getLong("delay.c", 0L);
+    private static final Integer ONE = 777;
+    MessagePassingQueue<Integer> q;
+    private Integer escape;
+    @Param(value = { "SpscArrayQueue", "MpscArrayQueue", "SpmcArrayQueue", "MpmcArrayQueue" })
+    String qType;
+    @Param(value = { "132000" })
+    int qCapacity;
 
-	@Setup()
-	public void createQandPrimeCompilation() {
-		q = MessagePassingQueueByTypeFactory.createQueue(qType, 128);
+    @Setup()
+    public void createQandPrimeCompilation() {
+        q = MessagePassingQueueByTypeFactory.createQueue(qType, 128);
 
-		// stretch the queue to the limit, working through resizing and full
-		for (int i = 0; i < 128 + 100; i++) {
-			q.offer(ONE);
-		}
-		for (int i = 0; i < 128 + 100; i++) {
-			q.poll();
-		}
-	    // stretch the queue to the limit, working through resizing and full
+        // stretch the queue to the limit, working through resizing and full
+        for (int i = 0; i < 128 + 100; i++) {
+            q.offer(ONE);
+        }
+        for (int i = 0; i < 128 + 100; i++) {
+            q.poll();
+        }
+        // stretch the queue to the limit, working through resizing and full
         for (int i = 0; i < 128 + 100; i++) {
             q.relaxedOffer(ONE);
         }
         for (int i = 0; i < 128 + 100; i++) {
             q.relaxedPoll();
         }
-		// make sure the important common case is exercised
-		for (int i = 0; i < 20000; i++) {
-		    q.offer(ONE);
+        // make sure the important common case is exercised
+        for (int i = 0; i < 20000; i++) {
+            q.offer(ONE);
             q.poll();
             q.relaxedOffer(ONE);
             q.relaxedPoll();
-		}
-		q = MessagePassingQueueByTypeFactory.createQueue(qType, qCapacity);
-	}
+        }
+        q = MessagePassingQueueByTypeFactory.createQueue(qType, qCapacity);
+    }
 
-	@AuxCounters
-	@State(Scope.Thread)
-	public static class PollCounters {
-		public int pollsFailed;
-		public int pollsMade;
+    @AuxCounters
+    @State(Scope.Thread)
+    public static class PollCounters {
+        public int pollsFailed;
+        public int pollsMade;
 
-		@Setup(Level.Iteration)
-		public void clean() {
-			pollsFailed = pollsMade = 0;
-		}
-	}
+        @Setup(Level.Iteration)
+        public void clean() {
+            pollsFailed = pollsMade = 0;
+        }
+    }
 
-	@AuxCounters
-	@State(Scope.Thread)
-	public static class OfferCounters {
-		public int offersFailed;
-		public int offersMade;
+    @AuxCounters
+    @State(Scope.Thread)
+    public static class OfferCounters {
+        public int offersFailed;
+        public int offersMade;
 
-		@Setup(Level.Iteration)
-		public void clean() {
-			offersFailed = offersMade = 0;
-		}
-	}
+        @Setup(Level.Iteration)
+        public void clean() {
+            offersFailed = offersMade = 0;
+        }
+    }
 
-	private static ThreadLocal<Object> marker = new ThreadLocal<Object>();
+    private static ThreadLocal<Object> marker = new ThreadLocal<Object>();
 
-	@State(Scope.Thread)
-	public static class ConsumerMarker {
-		public ConsumerMarker() {
-			marker.set(this);
-		}
-	}
+    @State(Scope.Thread)
+    public static class ConsumerMarker {
+        public ConsumerMarker() {
+            marker.set(this);
+        }
+    }
 
-	@Benchmark
-	@Group("nor")
-	public void offerNoR(OfferCounters counters) {
-		if (!q.offer(ONE)) {
-			counters.offersFailed++;
-			backoff();
-		} else {
-			counters.offersMade++;
-		}
-		if (DELAY_PRODUCER != 0) {
-			Blackhole.consumeCPU(DELAY_PRODUCER);
-		}
-	}
+    @Benchmark
+    @Group("nor")
+    public void offerNoR(OfferCounters counters) {
+        if (!q.offer(ONE)) {
+            counters.offersFailed++;
+            backoff();
+        } else {
+            counters.offersMade++;
+        }
+        if (DELAY_PRODUCER != 0) {
+            Blackhole.consumeCPU(DELAY_PRODUCER);
+        }
+    }
 
-	@Benchmark
-	@Group("nor")
-	public void pollNoR(PollCounters counters, ConsumerMarker cm) {
-		Integer e = q.poll();
-		if (e == null) {
-			counters.pollsFailed++;
-			backoff();
-		} else if (e == ONE) {
-			counters.pollsMade++;
-		} else {
-			escape = e;
-		}
-		if (DELAY_CONSUMER != 0) {
-			Blackhole.consumeCPU(DELAY_CONSUMER);
-		}
-	}
+    @Benchmark
+    @Group("nor")
+    public void pollNoR(PollCounters counters, ConsumerMarker cm) {
+        Integer e = q.poll();
+        if (e == null) {
+            counters.pollsFailed++;
+            backoff();
+        } else if (e == ONE) {
+            counters.pollsMade++;
+        } else {
+            escape = e;
+        }
+        if (DELAY_CONSUMER != 0) {
+            Blackhole.consumeCPU(DELAY_CONSUMER);
+        }
+    }
 
-	@Benchmark
-	@Group("bothr")
-	public void offerBothR(OfferCounters counters) {
-		if (!q.relaxedOffer(ONE)) {
-			counters.offersFailed++;
-			backoff();
-		} else {
-			counters.offersMade++;
-		}
-		if (DELAY_PRODUCER != 0) {
-			Blackhole.consumeCPU(DELAY_PRODUCER);
-		}
-	}
+    @Benchmark
+    @Group("bothr")
+    public void offerBothR(OfferCounters counters) {
+        if (!q.relaxedOffer(ONE)) {
+            counters.offersFailed++;
+            backoff();
+        } else {
+            counters.offersMade++;
+        }
+        if (DELAY_PRODUCER != 0) {
+            Blackhole.consumeCPU(DELAY_PRODUCER);
+        }
+    }
 
-	@Benchmark
-	@Group("bothr")
-	public void pollBothR(PollCounters counters, ConsumerMarker cm) {
-		Integer e = q.relaxedPoll();
-		if (e == null) {
-			counters.pollsFailed++;
-			backoff();
-		} else if (e == ONE) {
-			counters.pollsMade++;
-		} else {
-			escape = e;
-		}
-		if (DELAY_CONSUMER != 0) {
-			Blackhole.consumeCPU(DELAY_CONSUMER);
-		}
-	}
+    @Benchmark
+    @Group("bothr")
+    public void pollBothR(PollCounters counters, ConsumerMarker cm) {
+        Integer e = q.relaxedPoll();
+        if (e == null) {
+            counters.pollsFailed++;
+            backoff();
+        } else if (e == ONE) {
+            counters.pollsMade++;
+        } else {
+            escape = e;
+        }
+        if (DELAY_CONSUMER != 0) {
+            Blackhole.consumeCPU(DELAY_CONSUMER);
+        }
+    }
+
     @Benchmark
     @Group("pr")
     public void offerPR(OfferCounters counters) {
@@ -201,7 +202,7 @@ public class MpqThroughputBackoffNone {
             Blackhole.consumeCPU(DELAY_CONSUMER);
         }
     }
-    
+
     @Benchmark
     @Group("cr")
     public void offerCR(OfferCounters counters) {
@@ -232,17 +233,18 @@ public class MpqThroughputBackoffNone {
             Blackhole.consumeCPU(DELAY_CONSUMER);
         }
     }
-	@TearDown(Level.Iteration)
-	public void emptyQ() {
-		if (marker.get() == null)
-			return;
-		// sadly the iteration tear down is performed from each participating
-		// thread, so we need to guess
-		// which is which (can't have concurrent access to poll).
-		while (q.poll() != null)
-			;
-	}
 
-	protected void backoff() {
-	}
+    @TearDown(Level.Iteration)
+    public void emptyQ() {
+        if (marker.get() == null)
+            return;
+        // sadly the iteration tear down is performed from each participating
+        // thread, so we need to guess
+        // which is which (can't have concurrent access to poll).
+        while (q.poll() != null)
+            ;
+    }
+
+    protected void backoff() {
+    }
 }
