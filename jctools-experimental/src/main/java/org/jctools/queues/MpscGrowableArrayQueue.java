8845d49ff239d584ea7ed7de31b422229ccc325b
nitsanw <nitsanw@yahoo.com>
2016-02-27 15:46:32 +0200

    MpscGrowable implements MessagePassingQueue, drain/fill left unoptimized

--- jctools-experimental/src/main/java/org/jctools/queues/MpscGrowableArrayQueue.java
+++ jctools-experimental/src/main/java/org/jctools/queues/MpscGrowableArrayQueue.java
@@ -25,6 +25,10 @@ import java.lang.reflect.Field;
 import java.util.AbstractQueue;
 import java.util.Iterator;
 
+import org.jctools.queues.MessagePassingQueue.Consumer;
+import org.jctools.queues.MessagePassingQueue.ExitCondition;
+import org.jctools.queues.MessagePassingQueue.Supplier;
+import org.jctools.queues.MessagePassingQueue.WaitStrategy;
 import org.jctools.util.Pow2;
 
 abstract class MpscGrowableArrayQueuePad1<E> extends AbstractQueue<E> {
@@ -61,7 +65,7 @@ abstract class MpscGrowableArrayQueueConsumerFields<E> extends MpscGrowableArray
 }
 
 /**
- * An MPSC array queue which starts at <i>initialCapacity</i> and grows to <i>maxCapacity</i> in mutiples of
+ * An MPSC array queue which starts at <i>initialCapacity</i> and grows to <i>maxCapacity</i> in multiples of
  * 2. The queue grows only when the current buffer is full and elements are not copied on resize, instead a
  * link to the new buffer is stored in the old buffer for the consumer to follow.<br>
  *
@@ -69,7 +73,7 @@ abstract class MpscGrowableArrayQueueConsumerFields<E> extends MpscGrowableArray
  * @param <E>
  */
 public class MpscGrowableArrayQueue<E> extends MpscGrowableArrayQueueConsumerFields<E>
-        implements QueueProgressIndicators {
+        implements MessagePassingQueue<E>, QueueProgressIndicators {
     long p0, p1, p2, p3, p4, p5, p6, p7;
     long p10, p11, p12, p13, p14, p15, p16, p17;
     private final static long P_INDEX_OFFSET;
@@ -135,6 +139,7 @@ public class MpscGrowableArrayQueue<E> extends MpscGrowableArrayQueueConsumerFie
         if (null == e) {
             throw new NullPointerException("Null is not a valid element");
         }
+        final long maxQueueCapacity = this.maxQueueCapacity;
 
         long mask;
         E[] buffer;
@@ -152,8 +157,8 @@ public class MpscGrowableArrayQueue<E> extends MpscGrowableArrayQueueConsumerFie
             buffer = this.producerBuffer;
             final boolean atMaxCapacity = mask + 2 == maxQueueCapacity;
             final long currCapacity = atMaxCapacity ? mask + 1 : mask;
-
             final long wrapPoint = (currentProducerIndex - currCapacity);
+
             if (consumerIndexCache <= wrapPoint) {
                 consumerIndexCache = lvConsumerIndex();
                 if (consumerIndexCache > wrapPoint) {
@@ -177,7 +182,7 @@ public class MpscGrowableArrayQueue<E> extends MpscGrowableArrayQueueConsumerFie
             }
         }
 
-        final long offset = calcElementOffset(currentProducerIndex, mask);
+        final long offset = modifiedCalcElementOffset(currentProducerIndex, mask);
         soElement(buffer, offset, e);
         return true;
     }
@@ -187,10 +192,10 @@ public class MpscGrowableArrayQueue<E> extends MpscGrowableArrayQueueConsumerFie
         final E[] newBuffer = allocate(newCapacity + 1);
         producerBuffer = newBuffer;
         producerMask = (long) (newCapacity - 1) << 1;
-        final long offsetInOld = calcElementOffset(currentProducerIndex, mask);
-        final long offsetInNew = calcElementOffset(currentProducerIndex, producerMask);
-        spElement(buffer, nextArrayOffset(mask), newBuffer);
-        spElement(newBuffer, offsetInNew, e);
+        final long offsetInOld = modifiedCalcElementOffset(currentProducerIndex, mask);
+        final long offsetInNew = modifiedCalcElementOffset(currentProducerIndex, producerMask);
+        soElement(newBuffer, offsetInNew, e);
+        soElement(buffer, nextArrayOffset(mask), newBuffer);
 
         // make resize visible to consumer
         soElement(buffer, offsetInOld, JUMP);
@@ -199,7 +204,7 @@ public class MpscGrowableArrayQueue<E> extends MpscGrowableArrayQueueConsumerFie
         soProducerIndex(currentProducerIndex + 2);
     }
 
-    private static long calcElementOffset(long index, long mask) {
+    private static long modifiedCalcElementOffset(long index, long mask) {
         return REF_ARRAY_BASE + ((index & mask) << (REF_ELEMENT_SHIFT - 1));
     }
 
@@ -210,13 +215,19 @@ public class MpscGrowableArrayQueue<E> extends MpscGrowableArrayQueueConsumerFie
      */
     @SuppressWarnings("unchecked")
     @Override
-    public final E poll() {
+    public E poll() {
         final E[] buffer = consumerBuffer;
         final long index = consumerIndex;
         final long mask = consumerMask;
 
-        final long offset = calcElementOffset(index, mask);
-        final Object e = lvElement(buffer, offset);// LoadLoad
+        final long offset = modifiedCalcElementOffset(index, mask);
+        Object e = lvElement(buffer, offset);// LoadLoad
+        if (e == null && index != lvProducerIndex()) {
+            // poll() == null iff queue is empty, null element is not strong enough indicator, so we must
+            // check the producer index. If the queue is indeed not empty we spin until element is visible.
+            while ((e = lvElement(buffer, offset)) == null)
+                ;
+        }
         if (e != null) {
             if (e == JUMP) {
                 final E[] nextBuffer = getNextBuffer(buffer, mask);
@@ -235,13 +246,19 @@ public class MpscGrowableArrayQueue<E> extends MpscGrowableArrayQueueConsumerFie
      */
     @SuppressWarnings("unchecked")
     @Override
-    public final E peek() {
+    public E peek() {
         final E[] buffer = consumerBuffer;
         final long index = consumerIndex;
         final long mask = consumerMask;
 
-        final long offset = calcElementOffset(index, mask);
-        final Object e = lvElement(buffer, offset);// LoadLoad
+        final long offset = modifiedCalcElementOffset(index, mask);
+        Object e = lvElement(buffer, offset);// LoadLoad
+        if (e == null && index != lvProducerIndex()) {
+            // peek() == null iff queue is empty, null element is not strong enough indicator, so we must
+            // check the producer index. If the queue is indeed not empty we spin until element is visible.
+            while ((e = lvElement(buffer, offset)) == null)
+                ;
+        }
         if (e == JUMP) {
             return newBufferPeek(getNextBuffer(buffer, mask), index);
         }
@@ -254,13 +271,13 @@ public class MpscGrowableArrayQueue<E> extends MpscGrowableArrayQueueConsumerFie
     }
 
     private long nextArrayOffset(final long mask) {
-        return calcElementOffset(mask + 2, Long.MAX_VALUE);
+        return modifiedCalcElementOffset(mask + 2, Long.MAX_VALUE);
     }
 
     private E newBufferPoll(E[] nextBuffer, final long index) {
         final long offsetInNew = newBufferAndOffset(nextBuffer, index);
         final E n = lvElement(nextBuffer, offsetInNew);// LoadLoad
-        if (null == n) {
+        if (n == null) {
             throw new IllegalStateException("new buffer must have at least one element");
         }
         soElement(nextBuffer, offsetInNew, null);// StoreStore
@@ -280,7 +297,7 @@ public class MpscGrowableArrayQueue<E> extends MpscGrowableArrayQueueConsumerFie
     private long newBufferAndOffset(E[] nextBuffer, final long index) {
         consumerBuffer = nextBuffer;
         consumerMask = (nextBuffer.length - 2) << 1;
-        final long offsetInNew = calcElementOffset(index, consumerMask);
+        final long offsetInNew = modifiedCalcElementOffset(index, consumerMask);
         return offsetInNew;
     }
 
@@ -327,7 +344,7 @@ public class MpscGrowableArrayQueue<E> extends MpscGrowableArrayQueueConsumerFie
         return consumerIndexCache;
     }
 
-    protected final void soConsumerIndexCache(long v) {
+    private void soConsumerIndexCache(long v) {
         UNSAFE.putOrderedLong(this, C_INDEX_CACHE_OFFSET, v);
     }
 
@@ -339,5 +356,242 @@ public class MpscGrowableArrayQueue<E> extends MpscGrowableArrayQueueConsumerFie
     @Override
     public long currentConsumerIndex() {
         return lvConsumerIndex();
+    }
+
+    @Override
+    public int capacity() {
+        return (int) (maxQueueCapacity / 2);
+    }
+
+    @Override
+    public boolean relaxedOffer(E e) {
+        return offer(e);
+    }
+
+    @Override
+    public E relaxedPoll() {
+        final E[] buffer = consumerBuffer;
+        final long index = consumerIndex;
+        final long mask = consumerMask;
+
+        final long offset = modifiedCalcElementOffset(index, mask);
+        Object e = lvElement(buffer, offset);// LoadLoad
+        if (e != null) {
+            if (e == JUMP) {
+                final E[] nextBuffer = getNextBuffer(buffer, mask);
+                return newBufferPoll(nextBuffer, index);
+            }
+            soElement(buffer, offset, null);
+            soConsumerIndex(index + 2);
+        }
+        return (E) e;
+    }
+
+    @Override
+    public E relaxedPeek() {
+        final E[] buffer = consumerBuffer;
+        final long index = consumerIndex;
+        final long mask = consumerMask;
+
+        final long offset = modifiedCalcElementOffset(index, mask);
+        Object e = lvElement(buffer, offset);// LoadLoad
+
+        if (e == JUMP) {
+            return newBufferPeek(getNextBuffer(buffer, mask), index);
+        }
+        return (E) e;
+    }
+
+    @Override
+    public int drain(Consumer<E> c) {
+        return drain(c, capacity());
+    }
+
+    @Override
+    public int fill(Supplier<E> s) {
+        long result = 0;// result is a long because we want to have a safepoint check at regular intervals
+        final int capacity = capacity();
+        do {
+            final int filled = fill(s, MpmcArrayQueue.RECOMENDED_OFFER_BATCH);
+            if (filled == 0) {
+                return (int) result;
+            }
+            result += filled;
+        } while (result <= capacity);
+        return (int) result;
+    }
+
+    @Override
+    public int drain(final Consumer<E> c, final int limit) {
+        int i = 0;
+        E m;
+        for (; i < limit && (m = relaxedPoll()) != null; i++){
+          c.accept(m);
+        }
+        return i;
+//        E[] buffer = consumerBuffer;
+//        long mask = consumerMask;
+//        final long consumerIndex = this.consumerIndex;
+//
+//        for (int i = 0; i < limit; i++) {
+//            final long index = consumerIndex + 2 * i;
+//            final long offset = modifiedCalcElementOffset(index, mask);
+//            E e = lvElement(buffer, offset);// LoadLoad
+//            if (null == e) {
+//                return i;
+//            }
+//            if (e == JUMP) {
+//                final E[] nextBuffer = getNextBuffer(buffer, mask);
+//                e = newBufferPoll(nextBuffer, index);
+//                buffer = nextBuffer;
+//                mask = consumerMask;
+//            }
+//            else {
+//                soElement(buffer, offset, null);
+//                soConsumerIndex(index + 2);
+//            }
+//            c.accept(e);
+//        }
+//        return limit;
+    }
+
+    @Override
+    public int fill(Supplier<E> s, int limit) {
+        int i = 0;
+        for (i = 0; i < limit && relaxedOffer(s.get()); i++)
+            ;
+        return i;
+//        final long maxQueueCapacity = this.maxQueueCapacity;
+//        long mask;
+//        E[] buffer;
+//        long currentProducerIndex;
+//        long consumerIndexCache = lvConsumerIndexCache();
+//        int actualLimit_2 = 0;
+//        while (true) {
+//            // lower bit is indicative of resize, if we see it we spin until it's cleared
+//            while (((currentProducerIndex = lvProducerIndex()) & 1) == 1)
+//                ;
+//            // now we have a pIndex which is even (lower bit is 0)
+//
+//            long available_2 = maxQueueCapacity - (currentProducerIndex - consumerIndexCache);
+//            if (available_2 <= 0) {
+//                final long currConsumerIndex = lvConsumerIndex(); // LoadLoad
+//                available_2 += currConsumerIndex - consumerIndexCache;
+//                if (available_2 <= 0) {
+//                    return 0; // FULL :(
+//                }
+//                else {
+//                    // update shared cached value of the consumerIndex
+//                    soConsumerIndexCache(currConsumerIndex); // StoreLoad
+//                    // update on stack copy, we might need this value again if we lose the CAS.
+//                    consumerIndexCache = currConsumerIndex;
+//                }
+//            }
+//            actualLimit_2 = Math.min((int) available_2, limit * 2);
+//            // mask/buffer may get changed by resizing. Only used after successful CAS.
+//            mask = this.producerMask;
+//            buffer = this.producerBuffer;
+//            final boolean atMaxCapacity = mask + 2 == maxQueueCapacity;
+//            final long currCapacity = atMaxCapacity ? mask + 1 : mask;
+//            long currAvailable_2 = currCapacity - (currentProducerIndex - consumerIndexCache);
+//
+//            if (currAvailable_2 < actualLimit_2) {
+//                // potentially this resize is premature, should only happen if:
+//                // currAvailable_2 < actualLimit_2 -> requested fill limit exceed current buffer availability
+//                // currAvailable_2 < actualLimit_2 -> we are not at the max capacity
+//                // we resize and insert single element. We could try a batch insert here but this is a rare
+//                // event
+//                // and we do not want to prolong it by holding all producers up until the fill is done
+//                if (casProducerIndex(currentProducerIndex, currentProducerIndex + 1)) {
+//                    resize(currentProducerIndex, buffer, mask, s.get());
+//                    return 1;
+//                }
+//                else {
+//                    continue; // skip CAS, no point
+//                }
+//            }
+//            if (casProducerIndex(currentProducerIndex, currentProducerIndex + actualLimit_2)) {
+//                break;
+//            }
+//        }
+//
+//        // right, now we claimed a few slots and can fill them with goodness
+//        for (int i = 0; i < actualLimit_2; i += 2) {
+//            final long offset = modifiedCalcElementOffset(currentProducerIndex, mask);
+//            soElement(buffer, offset, s.get());
+//        }
+//        return actualLimit_2 / 2;
+    }
+
+    @Override
+    public void drain(Consumer<E> c, WaitStrategy w, ExitCondition exit) {
+        int idleCounter = 0;
+        while (exit.keepRunning()) {
+            E e = relaxedPoll();
+            if(e == null){
+                idleCounter = w.idle(idleCounter);
+                continue;
+            }
+            idleCounter = 0;
+            c.accept(e);
+        }
+//        E[] buffer = consumerBuffer;
+//        long mask = consumerMask;
+//        final long consumerIndex = this.consumerIndex;
+//
+//        while (exit.keepRunning()) {
+//            for (int i = 0; i < 4096; i++) {
+//                final long index = consumerIndex + 2 * i;
+//                final long offset = modifiedCalcElementOffset(index, mask);
+//                E e = lvElement(buffer, offset);// LoadLoad
+//                if (null == e) {
+//                    e = waitForElement(buffer, offset, w, exit);
+//                    if (e == null) {
+//                        return;
+//                    }
+//                }
+//                if (e == JUMP) {
+//                    final E[] nextBuffer = getNextBuffer(buffer, mask);
+//                    e = newBufferPoll(nextBuffer, index);
+//                    buffer = nextBuffer;
+//                    mask = consumerMask;
+//                }
+//                else {
+//                    soElement(buffer, offset, null);
+//                    soConsumerIndex(index + 2);
+//                }
+//                c.accept(e);
+//            }
+//        }
+    }
+
+    private E waitForElement(E[] buffer, final long offset, WaitStrategy w, ExitCondition exit) {
+        E e;
+        int counter = 0;
+        while ((e = lvElement(buffer, offset)) == null && exit.keepRunning()) {
+            counter = w.idle(counter);
+        }
+        return e;
+    }
+
+    @Override
+    public void fill(Supplier<E> s, WaitStrategy w, ExitCondition exit) {
+        int idleCounter = 0;
+        while (exit.keepRunning()) {
+            E e = s.get();
+            while (!relaxedOffer(e)) {
+                idleCounter = w.idle(idleCounter);
+                continue;
+            }
+            idleCounter = 0;
+        }
+//        int idleCounter = 0;
+//        while (exit.keepRunning()) {
+//            if (fill(s, MpmcArrayQueue.RECOMENDED_OFFER_BATCH) == 0) {
+//                idleCounter = w.idle(idleCounter);
+//                continue;
+//            }
+//            idleCounter = 0;
+//        }
     }
 }
