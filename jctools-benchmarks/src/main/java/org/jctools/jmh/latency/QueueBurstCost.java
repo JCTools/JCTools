abeb8d097eb5cbfc2352dc72a62bec39b41acc54
nitsanw <nitsanw@yahoo.com>
2016-03-29 07:47:50 +0200

    Fix package name

--- jctools-benchmarks/src/main/java/org/jctools/jmh/latency/QueueBurstCost.java
+++ jctools-benchmarks/src/main/java/org/jctools/jmh/latency/QueueBurstCost.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jctools.jmh.latency.spsc;
+package org.jctools.jmh.latency;
 
 import java.util.Queue;
 import java.util.concurrent.TimeUnit;
