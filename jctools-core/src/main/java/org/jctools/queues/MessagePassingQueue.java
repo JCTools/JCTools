c734f008e8e91cb0bbb183761277b09bac87b537
nitsanw <nitsanw@yahoo.com>
2016-09-23 18:12:22 +0200

    minor javadoc change

--- jctools-core/src/main/java/org/jctools/queues/MessagePassingQueue.java
+++ jctools-core/src/main/java/org/jctools/queues/MessagePassingQueue.java
@@ -21,9 +21,9 @@ import java.util.Queue;
  * Message passing queues provide happens before semantics to messages passed through, namely that writes made
  * by the producer before offering the message are visible to the consuming thread after the message has been
  * polled out of the queue.
- * 
+ *
  * @author nitsanw
- * 
+ *
  * @param <T>
  *            the event/message type
  */
@@ -38,7 +38,7 @@ public interface MessagePassingQueue<T> {
          * Users should be aware that underlying queue implementations may upfront claim parts of the queue
          * for batch operations and this will effect the view on the queue from the supplier method. In
          * particular size and any offer methods may take the view that the full batch has already happened.
-         * 
+         *
          * @return new element, NEVER null
          */
         T get();
@@ -53,7 +53,7 @@ public interface MessagePassingQueue<T> {
          * for batch operations and this will effect the view on the queue from the accept method. In
          * particular size and any poll/peek methods may take the view that the full batch has already
          * happened.
-         * 
+         *
          * @param e not null
          */
         void accept(T e);
@@ -63,7 +63,7 @@ public interface MessagePassingQueue<T> {
         /**
          * This method can implement static or dynamic backoff. Dynamic backoff will rely on the counter for
          * estimating how long the caller has been idling. The expected usage is:
-         * 
+         *
          * <pre>
          * <code>
          * int ic = 0;
@@ -77,7 +77,7 @@ public interface MessagePassingQueue<T> {
          * }
          * </code>
          * </pre>
-         * 
+         *
          * @param idleCounter idle calls counter, managed by the idle method until reset
          * @return new counter value to be used on subsequent idle cycle
          */
@@ -89,7 +89,7 @@ public interface MessagePassingQueue<T> {
         /**
          * This method should be implemented such that the flag read or determination cannot be hoisted out of
          * a loop which notmally means a volatile load, but with JDK9 VarHandles may mean getOpaque.
-         * 
+         *
          * @return true as long as we should keep running
          */
         boolean keepRunning();
@@ -98,7 +98,7 @@ public interface MessagePassingQueue<T> {
     /**
      * Called from a producer thread subject to the restrictions appropriate to the implementation and
      * according to the {@link Queue#offer(Object)} interface.
-     * 
+     *
      * @param e not null, will throw NPE if it is
      * @return true if element was inserted into the queue, false iff full
      */
@@ -107,7 +107,7 @@ public interface MessagePassingQueue<T> {
     /**
      * Called from the consumer thread subject to the restrictions appropriate to the implementation and
      * according to the {@link Queue#poll()} interface.
-     * 
+     *
      * @return a message from the queue if one is available, null iff empty
      */
     T poll();
@@ -115,7 +115,7 @@ public interface MessagePassingQueue<T> {
     /**
      * Called from the consumer thread subject to the restrictions appropriate to the implementation and
      * according to the {@link Queue#peek()} interface.
-     * 
+     *
      * @return a message from the queue if one is available, null iff empty
      */
     T peek();
@@ -124,7 +124,7 @@ public interface MessagePassingQueue<T> {
      * This method's accuracy is subject to concurrent modifications happening as the size is estimated and as
      * such is a best effort rather than absolute value. For some implementations this method may be O(n)
      * rather than O(1).
-     * 
+     *
      * @return number of messages in the queue, between 0 and {@link Integer#MAX_VALUE} but less or equals to
      *         capacity (if bounded).
      */
@@ -139,20 +139,20 @@ public interface MessagePassingQueue<T> {
     /**
      * This method's accuracy is subject to concurrent modifications happening as the observation is carried
      * out.
-     * 
+     *
      * @return true if empty, false otherwise
      */
     boolean isEmpty();
 
     /**
-     * @return the capacity of this queue or UNBOUNDED_CAPACITY if not bounded
+     * @return the capacity of this queue or {@link MessagePassingQueue#UNBOUNDED_CAPACITY} if not bounded
      */
     int capacity();
 
     /**
      * Called from a producer thread subject to the restrictions appropriate to the implementation. As opposed
      * to {@link Queue#offer(Object)} this method may return false without the queue being full.
-     * 
+     *
      * @param e not null, will throw NPE if it is
      * @return true if element was inserted into the queue, false if unable to offer
      */
@@ -161,7 +161,7 @@ public interface MessagePassingQueue<T> {
     /**
      * Called from the consumer thread subject to the restrictions appropriate to the implementation. As
      * opposed to {@link Queue#poll()} this method may return null without the queue being empty.
-     * 
+     *
      * @return a message from the queue if one is available, null if unable to poll
      */
     T relaxedPoll();
@@ -169,7 +169,7 @@ public interface MessagePassingQueue<T> {
     /**
      * Called from the consumer thread subject to the restrictions appropriate to the implementation. As
      * opposed to {@link Queue#peek()} this method may return null without the queue being empty.
-     * 
+     *
      * @return a message from the queue if one is available, null if unable to peek
      */
     T relaxedPeek();
@@ -183,7 +183,7 @@ public interface MessagePassingQueue<T> {
      * }</br>
      * </code> There's no strong commitment to the queue being empty at the end of a drain. Called from a
      * consumer thread subject to the restrictions appropriate to the implementation.
-     * 
+     *
      * @return the number of polled elements
      */
     int drain(Consumer<T> c);
@@ -193,7 +193,7 @@ public interface MessagePassingQueue<T> {
      * while(relaxedOffer(s.get());</br>
      * </code> There's no strong commitment to the queue being full at the end of a fill. Called from a
      * producer thread subject to the restrictions appropriate to the implementation.
-     * 
+     *
      * @return the number of offered elements
      */
     int fill(Supplier<T> s);
@@ -201,7 +201,7 @@ public interface MessagePassingQueue<T> {
     /**
      * Remove up to <i>limit</i> elements from the queue and hand to consume. This should be semantically
      * similar to:
-     * 
+     *
      * <pre>
      * <code>
      *   M m;
@@ -210,33 +210,33 @@ public interface MessagePassingQueue<T> {
      *   }
      * </code>
      * </pre>
-     * 
+     *
      * There's no strong commitment to the queue being empty at the end of a drain. Called from a consumer
      * thread subject to the restrictions appropriate to the implementation.
-     * 
+     *
      * @return the number of polled elements
      */
     int drain(Consumer<T> c, int limit);
 
     /**
      * Stuff the queue with up to <i>limit</i> elements from the supplier. Semantically similar to:
-     * 
+     *
      * <pre>
      * <code>
      *   for(int i=0; i < limit && relaxedOffer(s.get(); i++);</br>
      * </code>
      * </pre>
-     * 
+     *
      * There's no strong commitment to the queue being full at the end of a fill. Called from a producer
      * thread subject to the restrictions appropriate to the implementation.
-     * 
+     *
      * @return the number of offered elements
      */
     int fill(Supplier<T> s, int limit);
 
     /**
      * Remove elements from the queue and hand to consume forever. Semantically similar to:
-     * 
+     *
      * <pre>
      * <code>
      *  int idleCounter = 0;
@@ -251,15 +251,15 @@ public interface MessagePassingQueue<T> {
      *  }
      * </code>
      * </pre>
-     * 
+     *
      * Called from a consumer thread subject to the restrictions appropriate to the implementation.
-     * 
+     *
      */
     void drain(Consumer<T> c, WaitStrategy wait, ExitCondition exit);
 
     /**
      * Stuff the queue with elements from the supplier forever. Semantically similar to:
-     * 
+     *
      * <pre>
      * <code>
      *  int idleCounter = 0;
@@ -269,13 +269,13 @@ public interface MessagePassingQueue<T> {
      *          idleCounter = wait.idle(idleCounter);
      *          continue;
      *      }
-     *      idleCounter = 0;    
+     *      idleCounter = 0;
      *  }
      * </code>
      * </pre>
-     * 
+     *
      * Called from a producer thread subject to the restrictions appropriate to the implementation.
-     * 
+     *
      */
     void fill(Supplier<T> s, WaitStrategy wait, ExitCondition exit);
 }
