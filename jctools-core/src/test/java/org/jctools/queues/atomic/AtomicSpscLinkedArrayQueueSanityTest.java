947ad460a38a858263d2f709e6576f87fc5f2d82
neomatrix369 <sadhak001@gmail.com>
2017-05-11 17:31:14 +0100

    Enabled test for SpscUnboundedAtomicArrayQueue (already implemented)

--- jctools-core/src/test/java/org/jctools/queues/atomic/AtomicSpscLinkedArrayQueueSanityTest.java
+++ jctools-core/src/test/java/org/jctools/queues/atomic/AtomicSpscLinkedArrayQueueSanityTest.java
@@ -17,7 +17,7 @@ public class AtomicSpscLinkedArrayQueueSanityTest extends QueueSanityTest {
         ArrayList<Object[]> list = new ArrayList<Object[]>();
         list.add(makeQueue(1, 1, SIZE, Ordering.FIFO, new AtomicSpscChunkedArrayQueue<Integer>(64, SIZE)));
         list.add(makeQueue(1, 1, SIZE, Ordering.FIFO, new AtomicSpscGrowableArrayQueue<Integer>(64, SIZE)));
-        //list.add(makeQueue(1, 1, 0, Ordering.FIFO, new AtomicSpscUnboundedArrayQueue<Integer>(16)));
+        list.add(makeQueue(1, 1, 0, Ordering.FIFO, new SpscUnboundedAtomicArrayQueue<Integer>(16)));
         // minimal sizes
         list.add(makeQueue(1, 1, 16, Ordering.FIFO, new AtomicSpscChunkedArrayQueue<Integer>(8, 16)));
         list.add(makeQueue(1, 1, 16, Ordering.FIFO, new AtomicSpscGrowableArrayQueue<Integer>(8, 16)));
