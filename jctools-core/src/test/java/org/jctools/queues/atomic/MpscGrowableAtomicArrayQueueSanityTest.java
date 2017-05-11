2ef1677c2474edf317093c71a2fc4ebd81702549
Christopher Batey <christopher.batey@gmail.com>
2017-05-11 18:18:49 +0100

    Initial implementation of MPSC atomic chunked and growable queue
    Ported from the non-atomic verisons

---
+++ jctools-core/src/test/java/org/jctools/queues/atomic/MpscGrowableAtomicArrayQueueSanityTest.java
@@ -0,0 +1,27 @@
+package org.jctools.queues.atomic;
+
+import org.jctools.queues.MpscGrowableArrayQueue;
+import org.jctools.queues.QueueSanityTest;
+import org.jctools.queues.spec.ConcurrentQueueSpec;
+import org.jctools.queues.spec.Ordering;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Queue;
+
+@RunWith(Parameterized.class)
+public class MpscGrowableAtomicArrayQueueSanityTest extends QueueSanityTest {
+    @Parameterized.Parameters
+    public static Collection<Object[]> parameters() {
+        ArrayList<Object[]> list = new ArrayList<Object[]>();
+        list.add(makeQueue(0, 1, 4, Ordering.FIFO, new MpscGrowableAtomicArrayQueue<>(2, 4)));// MPSC size 1
+        list.add(makeQueue(0, 1, SIZE, Ordering.FIFO, new MpscGrowableAtomicArrayQueue<>(8, SIZE)));// MPSC size SIZE
+        return list;
+    }
+
+    public MpscGrowableAtomicArrayQueueSanityTest(ConcurrentQueueSpec spec, Queue<Integer> queue) {
+        super(spec, queue);
+    }
+}
