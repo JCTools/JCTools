c3ac4ace17c462fc133cba9233ce064ead945792
nitsanw <nitsanw@yahoo.com>
2017-08-09 14:30:02 +0200

    Format and mark as InternalAPI

--- jctools-core/src/main/java/org/jctools/queues/PaddedCircularArrayOffsetCalculator.java
+++ jctools-core/src/main/java/org/jctools/queues/PaddedCircularArrayOffsetCalculator.java
@@ -1,12 +1,17 @@
 package org.jctools.queues;
 
+import org.jctools.util.InternalAPI;
 import org.jctools.util.JvmInfo;
 import org.jctools.util.UnsafeRefArrayAccess;
 
-public final class PaddedCircularArrayOffsetCalculator {
+@InternalAPI
+public final class PaddedCircularArrayOffsetCalculator
+{
     static final int REF_BUFFER_PAD;
     static final long REF_ARRAY_BASE;
-    static {
+
+    static
+    {
         // 2 cache lines pad
         REF_BUFFER_PAD = (JvmInfo.CACHE_LINE_SIZE * 2) >> UnsafeRefArrayAccess.REF_ELEMENT_SHIFT;
         // Including the buffer pad in the array base offset
@@ -14,11 +19,9 @@ public final class PaddedCircularArrayOffsetCalculator {
         REF_ARRAY_BASE = UnsafeRefArrayAccess.REF_ARRAY_BASE + paddingOffset;
     }
 
-    private PaddedCircularArrayOffsetCalculator() {
-    }
-
     @SuppressWarnings("unchecked")
-    public static <E> E[] allocate(int capacity) {
+    public static <E> E[] allocate(int capacity)
+    {
         return (E[]) new Object[capacity + REF_BUFFER_PAD * 2];
     }
 
@@ -27,7 +30,8 @@ public final class PaddedCircularArrayOffsetCalculator {
      * @param mask
      * @return the offset in bytes within the array for a given index.
      */
-    protected static long calcElementOffset(long index, long mask) {
+    protected static long calcElementOffset(long index, long mask)
+    {
         return REF_ARRAY_BASE + ((index & mask) << UnsafeRefArrayAccess.REF_ELEMENT_SHIFT);
     }
 }
