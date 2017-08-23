JCTools/JCTools

commit 4e68fd268b6d936c6e73aacb1badaa48586d539f

Merge branch 'homogenize_atomic_linked_queues' of git://github.com/kay/JCTools into kay-homogenize_atomic_linked_queues
# Conflicts:
#	jctools-core/src/main/java/org/jctools/queues/MpscLinkedQueue.java
#	jctools-core/src/main/java/org/jctools/queues/atomic/AtomicReferenceArrayQueue.java
#	jctools-core/src/main/java/org/jctools/queues/atomic/BaseLinkedAtomicQueue.java
#	jctools-core/src/main/java/org/jctools/queues/atomic/BaseMpscLinkedAtomicArrayQueue.java
#	jctools-core/src/main/java/org/jctools/queues/atomic/MpscLinkedAtomicQueue.java
#	jctools-core/src/main/java/org/jctools/queues/atomic/SpscLinkedAtomicQueue.java

jctools-core/src/main/java/org/jctools/queues/atomic/MpscAtomicArrayQueue.java
@@ -13,6 +13,7 @@
  */
 package org.jctools.queues.atomic;
 
+import org.jctools.util.PortableJvmInfo;
 import java.util.concurrent.atomic.AtomicLongFieldUpdater;
 import java.util.concurrent.atomic.AtomicReferenceArray;
 import java.util.concurrent.atomic.AtomicLongArray;
@@ -428,7 +429,7 @@ public class MpscAtomicArrayQueue<E> extends MpscAtomicArrayQueueL3Pad<E> {
         long result = 0;
         final int capacity = capacity();
         do {
-            final int filled = fill(s, MpmcAtomicArrayQueue.RECOMENDED_OFFER_BATCH);
+            final int filled = fill(s, PortableJvmInfo.RECOMENDED_OFFER_BATCH);
             if (filled == 0) {
                 return (int) result;
             }
@@ -525,7 +526,7 @@ public class MpscAtomicArrayQueue<E> extends MpscAtomicArrayQueueL3Pad<E> {
     public void fill(Supplier<E> s, WaitStrategy w, ExitCondition exit) {
         int idleCounter = 0;
         while (exit.keepRunning()) {
-            if (fill(s, MpmcAtomicArrayQueue.RECOMENDED_OFFER_BATCH) == 0) {
+            if (fill(s, PortableJvmInfo.RECOMENDED_OFFER_BATCH) == 0) {
                 idleCounter = w.idle(idleCounter);
                 continue;
             }

jctools-core/src/main/java/org/jctools/queues/atomic/MpscLinkedAtomicQueue.java
@@ -13,28 +13,41 @@
  */
 package org.jctools.queues.atomic;
 
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+import java.util.concurrent.atomic.AtomicLongFieldUpdater;
+import org.jctools.queues.MessagePassingQueue;
+import org.jctools.queues.MessagePassingQueue.Supplier;
+import org.jctools.queues.MessagePassingQueueUtil;
+import org.jctools.queues.QueueProgressIndicators;
+import org.jctools.queues.IndexedQueueSizeUtil;
+import static org.jctools.queues.atomic.LinkedAtomicArrayQueueUtil.*;
+import java.util.concurrent.atomic.AtomicReferenceArray;
+import org.jctools.queues.MpmcArrayQueue;
+
 /**
- * This is a direct Java port of the MPSC algorithm as presented <a
- * href="http://www.1024cores.net/home/lock-free-algorithms/queues/non-intrusive-mpsc-node-based-queue"> on 1024
- * Cores</a> by D. Vyukov. The original has been adapted to Java and it's quirks with regards to memory model and
- * layout:
+ * NOTE: This class was automatically generated by org.jctools.queues.atomic.JavaParsingAtomicLinkedQueueGenerator
+ * which can found in the jctools-build module. The original source file is MpscLinkedQueue.java.
+ 
+ * This is a direct Java port of the MPSC algorithm as presented
+ * <a href="http://www.1024cores.net/home/lock-free-algorithms/queues/non-intrusive-mpsc-node-based-queue"> on
+ * 1024 Cores</a> by D. Vyukov. The original has been adapted to Java and it's quirks with regards to memory
+ * model and layout:
  * <ol>
- * <li>Use XCHG functionality provided by AtomicReference (which is better in JDK 8+).
+ * <li>Use inheritance to ensure no false sharing occurs between producer/consumer node reference fields.
+ * <li>Use XCHG functionality to the best of the JDK ability (see differences in JDK7/8 impls).
  * </ol>
- * The queue is initialized with a stub node which is set to both the producer and consumer node references. From this
- * point follow the notes on offer/poll.
+ * The queue is initialized with a stub node which is set to both the producer and consumer node references.
+ * From this point follow the notes on offer/poll.
  *
- * @param <E>
+ * @param <E> the type of elements in this queue
  * @author nitsanw
  */
-public final class MpscLinkedAtomicQueue<E> extends BaseLinkedAtomicQueue<E>
-{
-    public MpscLinkedAtomicQueue()
-    {
-        super();
-        LinkedQueueAtomicNode<E> node = new LinkedQueueAtomicNode<E>();
+public final class MpscLinkedAtomicQueue<E> extends BaseLinkedAtomicQueue<E> {
+
+    protected MpscLinkedAtomicQueue() {
+        LinkedQueueAtomicNode<E> node = newNode();
         spConsumerNode(node);
-        xchgProducerNode(node);// this ensures correct construction: StoreLoad
+        xchgProducerNode(node);
     }
 
     /**
@@ -47,24 +60,23 @@ public final class MpscLinkedAtomicQueue<E> extends BaseLinkedAtomicQueue<E>
      * <li>Swaps it atomically with current producer node (only one producer 'wins')
      * <li>Sets the new node as the node following from the swapped producer node
      * </ol>
-     * This works because each producer is guaranteed to 'plant' a new node and link the old node. No 2 producers can
-     * get the same producer node as part of XCHG guarantee.
+     * This works because each producer is guaranteed to 'plant' a new node and link the old node. No 2
+     * producers can get the same producer node as part of XCHG guarantee.
      *
-     * @see org.jctools.queues.MessagePassingQueue#offer(Object)
+     * @see MessagePassingQueue#offer(Object)
      * @see java.util.Queue#offer(java.lang.Object)
      */
     @Override
-    public final boolean offer(final E e)
-    {
-        if (null == e)
-        {
+    public final boolean offer(final E e) {
+        if (null == e) {
             throw new NullPointerException();
         }
-        final LinkedQueueAtomicNode<E> nextNode = new LinkedQueueAtomicNode<E>(e);
+        final LinkedQueueAtomicNode<E> nextNode = newNode(e);
         final LinkedQueueAtomicNode<E> prevProducerNode = xchgProducerNode(nextNode);
         // Should a producer thread get interrupted here the chain WILL be broken until that thread is resumed
         // and completes the store in prev.next.
-        prevProducerNode.soNext(nextNode); // StoreStore
+        // StoreStore
+        prevProducerNode.soNext(nextNode);
         return true;
     }
 
@@ -78,23 +90,21 @@ public final class MpscLinkedAtomicQueue<E> extends BaseLinkedAtomicQueue<E>
      * <li>If it is null, the queue is assumed empty (though it might not be).
      * <li>If it is not null set it as the consumer node and return it's now evacuated value.
      * </ol>
-     * This means the consumerNode.value is always null, which is also the starting point for the queue. Because null
-     * values are not allowed to be offered this is the only node with it's value set to null at any one time.
+     * This means the consumerNode.value is always null, which is also the starting point for the queue.
+     * Because null values are not allowed to be offered this is the only node with it's value set to null at
+     * any one time.
      *
-     * @see org.jctools.queues.MessagePassingQueue#poll()
+     * @see MessagePassingQueue#poll()
      * @see java.util.Queue#poll()
      */
     @Override
-    public final E poll()
-    {
-        LinkedQueueAtomicNode<E> currConsumerNode = lpConsumerNode(); // don't load twice, it's alright
+    public final E poll() {
+        // don't load twice, it's alright
+        LinkedQueueAtomicNode<E> currConsumerNode = lpConsumerNode();
         LinkedQueueAtomicNode<E> nextNode = currConsumerNode.lvNext();
-        if (nextNode != null)
-        {
+        if (nextNode != null) {
             return getSingleConsumerNodeValue(currConsumerNode, nextNode);
-        }
-        else if (currConsumerNode != lvProducerNode())
-        {
+        } else if (currConsumerNode != lvProducerNode()) {
             nextNode = spinWaitForNextNode(currConsumerNode);
             // got the next node...
             return getSingleConsumerNodeValue(currConsumerNode, nextNode);
@@ -103,16 +113,13 @@ public final class MpscLinkedAtomicQueue<E> extends BaseLinkedAtomicQueue<E>
     }
 
     @Override
-    public final E peek()
-    {
-        LinkedQueueAtomicNode<E> currConsumerNode = lpConsumerNode(); // don't load twice, it's alright
+    public final E peek() {
+        // don't load twice, it's alright
+        LinkedQueueAtomicNode<E> currConsumerNode = lpConsumerNode();
         LinkedQueueAtomicNode<E> nextNode = currConsumerNode.lvNext();
-        if (nextNode != null)
-        {
+        if (nextNode != null) {
             return nextNode.lpValue();
-        }
-        else if (currConsumerNode != lvProducerNode())
-        {
+        } else if (currConsumerNode != lvProducerNode()) {
             nextNode = spinWaitForNextNode(currConsumerNode);
             // got the next node...
             return nextNode.lpValue();
@@ -127,48 +134,37 @@ public final class MpscLinkedAtomicQueue<E> extends BaseLinkedAtomicQueue<E>
      * with producers.
      */
     @Override
-    public final boolean remove(Object o)
-    {
-        if (null == o)
-        {
-            return false; // Null elements are not permitted, so null will never be removed.
+    public final boolean remove(Object o) {
+        if (null == o) {
+            // Null elements are not permitted, so null will never be removed.
+            return false;
         }
-
         final LinkedQueueAtomicNode<E> originalConsumerNode = lpConsumerNode();
         LinkedQueueAtomicNode<E> prevConsumerNode = originalConsumerNode;
         LinkedQueueAtomicNode<E> currConsumerNode = getNextConsumerNode(originalConsumerNode);
-        while (currConsumerNode != null)
-        {
-            if (o.equals(currConsumerNode.lpValue()))
-            {
+        while (currConsumerNode != null) {
+            if (o.equals(currConsumerNode.lpValue())) {
                 LinkedQueueAtomicNode<E> nextNode = getNextConsumerNode(currConsumerNode);
                 // e.g.: consumerNode -> node0 -> node1(o==v) -> node2 ... => consumerNode -> node0 -> node2
-                if (nextNode != null)
-                {
+                if (nextNode != null) {
                     // We are removing an interior node.
                     prevConsumerNode.soNext(nextNode);
-
-                }
-                // This case reflects: prevConsumerNode != originalConsumerNode && nextNode == null
+                } else // This case reflects: prevConsumerNode != originalConsumerNode && nextNode == null
                 // At rest, this would be the producerNode, but we must contend with racing. Changes to subclassed
                 // queues need to consider remove() when implementing offer().
-                else
                 {
                     // producerNode is currConsumerNode, try to atomically update the reference to move it to the
                     // previous node.
                     prevConsumerNode.soNext(null);
-                    if (!casProducerNode(currConsumerNode, prevConsumerNode))
-                    {
+                    if (!casProducerNode(currConsumerNode, prevConsumerNode)) {
                         // If the producer(s) have offered more items we need to remove the currConsumerNode link.
                         nextNode = spinWaitForNextNode(currConsumerNode);
                         prevConsumerNode.soNext(nextNode);
                     }
                 }
-
                 // Avoid GC nepotism because we are discarding the current node.
                 currConsumerNode.soNext(null);
                 currConsumerNode.spValue(null);
-
                 return true;
             }
             prevConsumerNode = currConsumerNode;
@@ -177,27 +173,52 @@ public final class MpscLinkedAtomicQueue<E> extends BaseLinkedAtomicQueue<E>
         return false;
     }
 
-    private boolean casProducerNode(LinkedQueueAtomicNode<E> oldVal, LinkedQueueAtomicNode<E> newVal)
-    {
-        return producerNode.compareAndSet(oldVal, newVal);
+    @Override
+    public int fill(Supplier<E> s) {
+        // result is a long because we want to have a safepoint check at regular intervals
+        long result = 0;
+        do {
+            fill(s, 4096);
+            result += 4096;
+        } while (result <= Integer.MAX_VALUE - 4096);
+        return (int) result;
     }
 
-    private LinkedQueueAtomicNode<E> getNextConsumerNode(LinkedQueueAtomicNode<E> currConsumerNode)
-    {
+    @Override
+    public int fill(Supplier<E> s, int limit) {
+        if (limit == 0)
+            return 0;
+        LinkedQueueAtomicNode<E> tail = newNode(s.get());
+        final LinkedQueueAtomicNode<E> head = tail;
+        for (int i = 1; i < limit; i++) {
+            final LinkedQueueAtomicNode<E> temp = newNode(s.get());
+            tail.soNext(temp);
+            tail = temp;
+        }
+        final LinkedQueueAtomicNode<E> oldPNode = xchgProducerNode(tail);
+        oldPNode.soNext(head);
+        return limit;
+    }
+
+    @Override
+    public void fill(Supplier<E> s, WaitStrategy wait, ExitCondition exit) {
+        while (exit.keepRunning()) {
+            fill(s, 4096);
+        }
+    }
+
+    private LinkedQueueAtomicNode<E> getNextConsumerNode(LinkedQueueAtomicNode<E> currConsumerNode) {
         LinkedQueueAtomicNode<E> nextNode = currConsumerNode.lvNext();
-        if (nextNode == null && currConsumerNode != lvProducerNode())
-        {
+        if (nextNode == null && currConsumerNode != lvProducerNode()) {
             nextNode = spinWaitForNextNode(currConsumerNode);
         }
         return nextNode;
     }
 
-    private LinkedQueueAtomicNode<E> spinWaitForNextNode(LinkedQueueAtomicNode<E> currNode)
-    {
+    private LinkedQueueAtomicNode<E> spinWaitForNextNode(LinkedQueueAtomicNode<E> currNode) {
         LinkedQueueAtomicNode<E> nextNode;
-        while ((nextNode = currNode.lvNext()) == null)
-        {
-            // spin, we are no longer wait free
+        while ((nextNode = currNode.lvNext()) == null) {
+        // spin, we are no longer wait free
         }
         return nextNode;
     }

jctools-core/src/main/java/org/jctools/queues/BaseLinkedQueue.java
@@ -49,6 +49,11 @@ abstract class BaseLinkedQueueProducerNodeRef<E> extends BaseLinkedQueuePad0<E> 
         return (LinkedQueueNode<E>) UNSAFE.getObjectVolatile(this, P_NODE_OFFSET);
     }
 
+    @SuppressWarnings("unchecked")
+    protected final boolean casProducerNode(LinkedQueueNode<E> expect, LinkedQueueNode<E> newValue) {
+        return UNSAFE.compareAndSwapObject(this, P_NODE_OFFSET, expect, newValue);
+    }
+
     protected final LinkedQueueNode<E> lpProducerNode() {
         return producerNode;
     }

jctools-core/src/main/java/org/jctools/queues/BaseMpscLinkedArrayQueue.java
@@ -25,6 +25,7 @@ import java.util.AbstractQueue;
 import java.util.Iterator;
 
 import org.jctools.queues.IndexedQueueSizeUtil.IndexedQueue;
+import org.jctools.util.PortableJvmInfo;
 import org.jctools.util.Pow2;
 import org.jctools.util.RangeUtil;
 
@@ -526,7 +527,7 @@ public abstract class BaseMpscLinkedArrayQueue<E> extends BaseMpscLinkedArrayQue
         final int capacity = capacity();
         do
         {
-            final int filled = fill(s, MpmcArrayQueue.RECOMENDED_OFFER_BATCH);
+            final int filled = fill(s, PortableJvmInfo.RECOMENDED_OFFER_BATCH);
             if (filled == 0)
             {
                 return (int) result;
@@ -606,12 +607,12 @@ public abstract class BaseMpscLinkedArrayQueue<E> extends BaseMpscLinkedArrayQue
 
         while (exit.keepRunning())
         {
-            while (fill(s, MpmcArrayQueue.RECOMENDED_OFFER_BATCH) != 0 && exit.keepRunning())
+            while (fill(s, PortableJvmInfo.RECOMENDED_OFFER_BATCH) != 0 && exit.keepRunning())
             {
                 continue;
             }
             int idleCounter = 0;
-            while (exit.keepRunning() && fill(s, MpmcArrayQueue.RECOMENDED_OFFER_BATCH) == 0)
+            while (exit.keepRunning() && fill(s, PortableJvmInfo.RECOMENDED_OFFER_BATCH) == 0)
             {
                 idleCounter = w.idle(idleCounter);
             }

jctools-core/src/main/java/org/jctools/queues/BaseSpscLinkedArrayQueue.java
@@ -25,6 +25,7 @@ import java.util.AbstractQueue;
 import java.util.Iterator;
 
 import org.jctools.queues.IndexedQueueSizeUtil.IndexedQueue;
+import org.jctools.util.PortableJvmInfo;
 
 abstract class BaseSpscLinkedArrayQueuePrePad<E> extends AbstractQueue<E> implements IndexedQueue
 {
@@ -203,7 +204,7 @@ abstract class BaseSpscLinkedArrayQueue<E> extends BaseSpscLinkedArrayQueueProdu
         final int capacity = capacity();
         do
         {
-            final int filled = fill(s, MpmcArrayQueue.RECOMENDED_OFFER_BATCH);
+            final int filled = fill(s, PortableJvmInfo.RECOMENDED_OFFER_BATCH);
             if (filled == 0)
             {
                 return (int) result;
@@ -257,12 +258,12 @@ abstract class BaseSpscLinkedArrayQueue<E> extends BaseSpscLinkedArrayQueueProdu
     {
         while (exit.keepRunning())
         {
-            while (fill(s, MpmcArrayQueue.RECOMENDED_OFFER_BATCH) != 0 && exit.keepRunning())
+            while (fill(s, PortableJvmInfo.RECOMENDED_OFFER_BATCH) != 0 && exit.keepRunning())
             {
                 continue;
             }
             int idleCounter = 0;
-            while (exit.keepRunning() && fill(s, MpmcArrayQueue.RECOMENDED_OFFER_BATCH) == 0)
+            while (exit.keepRunning() && fill(s, PortableJvmInfo.RECOMENDED_OFFER_BATCH) == 0)
             {
                 idleCounter = wait.idle(idleCounter);
             }

jctools-core/src/main/java/org/jctools/queues/MpmcArrayQueue.java
@@ -13,11 +13,11 @@
  */
 package org.jctools.queues;
 
-import static org.jctools.util.PortableJvmInfo.CPUs;
 import static org.jctools.util.UnsafeAccess.UNSAFE;
 import static org.jctools.util.UnsafeRefArrayAccess.lpElement;
 import static org.jctools.util.UnsafeRefArrayAccess.soElement;
 
+import org.jctools.util.PortableJvmInfo;
 import org.jctools.util.RangeUtil;
 
 abstract class MpmcArrayQueueL1Pad<E> extends ConcurrentSequencedCircularArrayQueue<E> {
@@ -124,9 +124,7 @@ abstract class MpmcArrayQueueL3Pad<E> extends MpmcArrayQueueConsumerIndexField<E
  *            type of the element stored in the {@link java.util.Queue}
  */
 public class MpmcArrayQueue<E> extends MpmcArrayQueueL3Pad<E> {
-    final static int RECOMENDED_POLL_BATCH = CPUs * 4;
-    public final static int RECOMENDED_OFFER_BATCH = CPUs * 4;
-    
+
     public MpmcArrayQueue(final int capacity) {
         super(RangeUtil.checkGreaterThanOrEqual(capacity, 2, "capacity"));
     }
@@ -285,7 +283,7 @@ public class MpmcArrayQueue<E> extends MpmcArrayQueueL3Pad<E> {
         int sum = 0;
         while (sum < capacity) {
             int drained = 0;
-            if((drained = drain(c, MpmcArrayQueue.RECOMENDED_POLL_BATCH)) == 0) {
+            if((drained = drain(c, PortableJvmInfo.RECOMENDED_POLL_BATCH)) == 0) {
                 break;
             }
             sum+=drained;
@@ -298,7 +296,7 @@ public class MpmcArrayQueue<E> extends MpmcArrayQueueL3Pad<E> {
         long result = 0;// result is a long because we want to have a safepoint check at regular intervals
         final int capacity = capacity();
         do {
-            final int filled = fill(s, RECOMENDED_OFFER_BATCH);
+            final int filled = fill(s, PortableJvmInfo.RECOMENDED_OFFER_BATCH);
             if (filled == 0) {
                 return (int) result;
             }
@@ -370,7 +368,7 @@ public class MpmcArrayQueue<E> extends MpmcArrayQueueL3Pad<E> {
             ExitCondition exit) {
         int idleCounter = 0;
         while (exit.keepRunning()) {
-            if(drain(c, MpmcArrayQueue.RECOMENDED_POLL_BATCH) == 0) {
+            if(drain(c, PortableJvmInfo.RECOMENDED_POLL_BATCH) == 0) {
                 idleCounter = w.idle(idleCounter);
                 continue;
             }
@@ -384,7 +382,7 @@ public class MpmcArrayQueue<E> extends MpmcArrayQueueL3Pad<E> {
             ExitCondition exit) {
         int idleCounter = 0;
         while (exit.keepRunning()) {
-            if (fill(s, MpmcArrayQueue.RECOMENDED_OFFER_BATCH) == 0) {
+            if (fill(s, PortableJvmInfo.RECOMENDED_OFFER_BATCH) == 0) {
                 idleCounter = w.idle(idleCounter);
                 continue;
             }

jctools-core/src/main/java/org/jctools/queues/MpscArrayQueue.java
@@ -13,6 +13,8 @@
  */
 package org.jctools.queues;
 
+import org.jctools.util.PortableJvmInfo;
+
 import static org.jctools.util.UnsafeAccess.UNSAFE;
 import static org.jctools.util.UnsafeRefArrayAccess.lvElement;
 import static org.jctools.util.UnsafeRefArrayAccess.soElement;
@@ -412,7 +414,7 @@ public class MpscArrayQueue<E> extends MpscArrayQueueL3Pad<E> {
         long result = 0;// result is a long because we want to have a safepoint check at regular intervals
         final int capacity = capacity();
         do {
-            final int filled = fill(s, MpmcArrayQueue.RECOMENDED_OFFER_BATCH);
+            final int filled = fill(s, PortableJvmInfo.RECOMENDED_OFFER_BATCH);
             if (filled == 0) {
                 return (int) result;
             }
@@ -503,7 +505,7 @@ public class MpscArrayQueue<E> extends MpscArrayQueueL3Pad<E> {
     public void fill(Supplier<E> s, WaitStrategy w, ExitCondition exit) {
         int idleCounter = 0;
         while (exit.keepRunning()) {
-            if (fill(s, MpmcArrayQueue.RECOMENDED_OFFER_BATCH) == 0) {
+            if (fill(s, PortableJvmInfo.RECOMENDED_OFFER_BATCH) == 0) {
                 idleCounter = w.idle(idleCounter);
                 continue;
             }

jctools-core/src/main/java/org/jctools/queues/MpscLinkedQueue.java
@@ -39,6 +39,7 @@ public abstract class MpscLinkedQueue<E> extends BaseLinkedQueue<E>
      *
      * @return the right queue for you!
      */
+    // $gen:ignore
     public static <E> MpscLinkedQueue<E> newMpscLinkedQueue()
     {
         if (UnsafeAccess.SUPPORTS_GET_AND_SET)
@@ -239,11 +240,6 @@ public abstract class MpscLinkedQueue<E> extends BaseLinkedQueue<E>
 
     // $gen:ignore
     protected abstract LinkedQueueNode<E> xchgProducerNode(LinkedQueueNode<E> nextNode);
-
-    private boolean casProducerNode(LinkedQueueNode<E> oldVal, LinkedQueueNode<E> newVal)
-    {
-        return UNSAFE.compareAndSwapObject(this, P_NODE_OFFSET, oldVal, newVal);
-    }
 
     private LinkedQueueNode<E> getNextConsumerNode(LinkedQueueNode<E> currConsumerNode)
     {

commit 03ed26e269e6a8a7a1c508475d417d1d85dabed3

MpscLinkedAtomicQueue API breakage
Motivation:
4e68fd268b6d936c6e73aacb1badaa48586d539f introduced a new code generation utility. However this commit removed the public constructor for MpscLinkedAtomicQueue because MpscLinkedQueue has a static factory method and a protected constructor.

Modifications:
- Restore the public constructor in MpscLinkedAtomicQueue

Result:
Original API restored for MpscLinkedAtomicQueue.

jctools-build/src/main/java/org/jctools/queues/atomic/JavaParsingAtomicLinkedQueueGenerator.java
@@ -50,6 +50,7 @@ import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 public final class JavaParsingAtomicLinkedQueueGenerator extends VoidVisitorAdapter<Void> {
     private static final String GEN_DIRECTIVE_CLASS_CONTAINS_ORDERED_FIELD_ACCESSORS = "$gen:ordered-fields";
     private static final String GEN_DIRECTIVE_METHOD_IGNORE = "$gen:ignore";
+    private static final String MPSC_LINKED_ATOMIC_QUEUE_NAME = "MpscLinkedAtomicQueue";
     private static final String INDENT_LEVEL = "    ";
     private final String sourceFileName;
 
@@ -83,7 +84,7 @@ public final class JavaParsingAtomicLinkedQueueGenerator extends VoidVisitorAdap
         replaceParentClassesForAtomics(node);
 
         node.setName(translateQueueName(node.getNameAsString()));
-        if ("MpscLinkedAtomicQueue".equals(node.getNameAsString())) {
+        if (MPSC_LINKED_ATOMIC_QUEUE_NAME.equals(node.getNameAsString())) {
             /*
              * Special case for MPSC
              */
@@ -115,6 +116,11 @@ public final class JavaParsingAtomicLinkedQueueGenerator extends VoidVisitorAdap
         super.visit(n, arg);
         // Update the ctor to match the class name
         n.setName(translateQueueName(n.getNameAsString()));
+        if (MPSC_LINKED_ATOMIC_QUEUE_NAME.equals(n.getNameAsString())) {
+            // Special case for MPSC because the Unsafe variant has a static factory method and a protected constructor.
+            n.setModifier(Modifier.PROTECTED, false);
+            n.setModifier(Modifier.PUBLIC, true);
+        }
     }
 
     @Override

jctools-core/src/main/java/org/jctools/queues/atomic/MpscAtomicArrayQueue.java
@@ -232,7 +232,7 @@ public class MpscAtomicArrayQueue<E> extends MpscAtomicArrayQueueL3Pad<E> {
      * Lock free offer using a single CAS. As class name suggests access is permitted to many threads
      * concurrently.
      *
-     * @see java.util.Queue#offer   
+     * @see java.util.Queue#offer
      * @see org.jctools.queues.MessagePassingQueue#offer
      */
     @Override

jctools-core/src/main/java/org/jctools/queues/atomic/MpscLinkedAtomicQueue.java
@@ -44,7 +44,7 @@ import org.jctools.queues.MpmcArrayQueue;
  */
 public final class MpscLinkedAtomicQueue<E> extends BaseLinkedAtomicQueue<E> {
 
-    protected MpscLinkedAtomicQueue() {
+    public MpscLinkedAtomicQueue() {
         LinkedQueueAtomicNode<E> node = newNode();
         spConsumerNode(node);
         xchgProducerNode(node);