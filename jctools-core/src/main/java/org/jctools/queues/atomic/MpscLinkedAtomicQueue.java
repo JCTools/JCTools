dab43bc0d0fd3f9f0d2f75361f471099b771c953
nitsanw <nitsanw@yahoo.com>
2018-03-09 18:42:54 +0200

    No need for queues to be final (fix #208)

--- jctools-core/src/main/java/org/jctools/queues/atomic/MpscLinkedAtomicQueue.java
+++ jctools-core/src/main/java/org/jctools/queues/atomic/MpscLinkedAtomicQueue.java
@@ -42,7 +42,7 @@ import org.jctools.queues.MpmcArrayQueue;
  * @param <E> the type of elements in this queue
  * @author nitsanw
  */
-public final class MpscLinkedAtomicQueue<E> extends BaseLinkedAtomicQueue<E> {
+public class MpscLinkedAtomicQueue<E> extends BaseLinkedAtomicQueue<E> {
 
     public MpscLinkedAtomicQueue() {
         LinkedQueueAtomicNode<E> node = newNode();
