b36ee2a19c29fd6e8bed9f41cc082ceca2f4f59b
nickhill <nickhill@us.ibm.com>
2019-06-11 14:39:00 -0700

    Add MpscBlockingConsumerArrayQueue::poll(long, TimeUnit) method

--- jctools-core/src/main/java/org/jctools/queues/MpscBlockingConsumerArrayQueue.java
+++ jctools-core/src/main/java/org/jctools/queues/MpscBlockingConsumerArrayQueue.java
@@ -15,6 +15,7 @@ package org.jctools.queues;
 
 import java.util.AbstractQueue;
 import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
 
 import org.jctools.queues.IndexedQueueSizeUtil.IndexedQueue;
@@ -361,7 +362,7 @@ public class MpscBlockingConsumerArrayQueue<E> extends MpscBlockingConsumerArray
         Object e = lvElement(buffer, offset);// LoadLoad
         if (e == null)
         {
-            long pIndex = lvProducerIndex();
+            final long pIndex = lvProducerIndex();
             if (cIndex == pIndex && casProducerIndex(pIndex, pIndex + 1))
             {
                 // producers only try a wakeup when both the index and the blocked thread are visible
@@ -386,7 +387,75 @@ public class MpscBlockingConsumerArrayQueue<E> extends MpscBlockingConsumerArray
         
         return (E) e;
     }
-    
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This implementation is correct for single consumer thread use only.
+     */
+    @SuppressWarnings("unchecked")
+    public E poll(long timeout, TimeUnit unit) throws InterruptedException
+    {
+        final E[] buffer = consumerBuffer;
+        final long mask = consumerMask;
+        
+        final long cIndex = lpConsumerIndex();
+        final long offset = modifiedCalcElementOffset(cIndex, mask);
+        Object e = lvElement(buffer, offset);// LoadLoad
+        if (e == null)
+        {
+            final long pIndex = lvProducerIndex();
+            if (cIndex == pIndex && casProducerIndex(pIndex, pIndex + 1))
+            {
+                boolean unblocked = false;
+                try
+                {
+                    // producers only try a wakeup when both the index and the blocked thread are visible
+                    soBlocked(Thread.currentThread());
+                    long remaining = unit.toNanos(timeout);
+                    final long deadline = System.nanoTime() + remaining;
+                    while (true)
+                    {
+                        LockSupport.parkNanos(this, remaining);
+                        if (Thread.interrupted())
+                        {
+                            throw new InterruptedException();
+                        }
+                        if (lvBlocked() == null)
+                        {
+                            break;
+                        }
+                        remaining = deadline - System.nanoTime();
+                        if (remaining <= 0)
+                        {
+                            return null;
+                        }
+                    }
+                    unblocked = true;
+                }
+                finally
+                {
+                    if (!unblocked)
+                    {
+                        // revert blocking state
+                        if (casProducerIndex(pIndex + 1, pIndex))
+                        {
+                            soBlocked(null);
+                        }
+                    }
+                }
+            }
+            // producer index is visible before element, so if we wake up between the index moving and the element
+            // store we could see a null.
+            e = spinWaitForElement(buffer, offset);
+        }
+
+        soElement(buffer, offset, null); // release element null
+        soConsumerIndex(cIndex + 2); // release cIndex
+        
+        return (E) e;
+    }
+
     /**
      * {@inheritDoc}
      * <p>
