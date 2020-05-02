9157a5c593acec846b2ebbf8733c2528330dec14
Nitsan <nwakart@gmail.com>
2020-05-02 15:52:56 +0200

    Use `byte` fields for padding

--- jctools-experimental/src/main/java/org/jctools/counters/FixedSizeStripedLongCounter.java
+++ jctools-experimental/src/main/java/org/jctools/counters/FixedSizeStripedLongCounter.java
@@ -14,8 +14,22 @@ import org.jctools.util.Pow2;
  * @author Tolstopyatov Vsevolod
  */
 abstract class FixedSizeStripedLongCounterPrePad {
-    long l01, l02, l03, l04, l05, l06, l07, l08;
-    long l9, l10, l11, l12, l13, l14, l15;
+    byte b000,b001,b002,b003,b004,b005,b006,b007;//  8b
+    byte b010,b011,b012,b013,b014,b015,b016,b017;// 16b
+    byte b020,b021,b022,b023,b024,b025,b026,b027;// 24b
+    byte b030,b031,b032,b033,b034,b035,b036,b037;// 32b
+    byte b040,b041,b042,b043,b044,b045,b046,b047;// 40b
+    byte b050,b051,b052,b053,b054,b055,b056,b057;// 48b
+    byte b060,b061,b062,b063,b064,b065,b066,b067;// 56b
+    byte b070,b071,b072,b073,b074,b075,b076,b077;// 64b
+    byte b100,b101,b102,b103,b104,b105,b106,b107;// 72b
+    byte b110,b111,b112,b113,b114,b115,b116,b117;// 80b
+    byte b120,b121,b122,b123,b124,b125,b126,b127;// 88b
+    byte b130,b131,b132,b133,b134,b135,b136,b137;// 96b
+    byte b140,b141,b142,b143,b144,b145,b146,b147;//104b
+    byte b150,b151,b152,b153,b154,b155,b156,b157;//112b
+    byte b160,b161,b162,b163,b164,b165,b166,b167;//120b
+    // byte b170,b171,b172,b173,b174,b175,b176,b177;//128b
 }
 abstract class FixedSizeStripedLongCounterFields extends FixedSizeStripedLongCounterPrePad {
     protected static final int CACHE_LINE_IN_LONGS = PortableJvmInfo.CACHE_LINE_SIZE / 8;
@@ -23,7 +37,7 @@ abstract class FixedSizeStripedLongCounterFields extends FixedSizeStripedLongCou
     protected static final long COUNTER_ARRAY_BASE = Math.max(UNSAFE.arrayBaseOffset(long[].class), PortableJvmInfo.CACHE_LINE_SIZE - 8);
     // element shift is enlarged to include the padding, still aligned to long
     protected static final long ELEMENT_SHIFT = Integer.numberOfTrailingZeros(PortableJvmInfo.CACHE_LINE_SIZE);
-    
+
     // we pad each element in the array to effectively write a counter in each cache line
     protected final long[] cells;
     protected final int mask;
@@ -38,8 +52,22 @@ abstract class FixedSizeStripedLongCounterFields extends FixedSizeStripedLongCou
 }
 
 public abstract class FixedSizeStripedLongCounter extends FixedSizeStripedLongCounterFields implements Counter {
-    long l02, l03, l04, l05, l06, l07, l08;
-    long l9, l10, l11, l12, l13, l14, l15, l16;
+    byte b000,b001,b002,b003,b004,b005,b006,b007;//  8b
+    byte b010,b011,b012,b013,b014,b015,b016,b017;// 16b
+    byte b020,b021,b022,b023,b024,b025,b026,b027;// 24b
+    byte b030,b031,b032,b033,b034,b035,b036,b037;// 32b
+    byte b040,b041,b042,b043,b044,b045,b046,b047;// 40b
+    byte b050,b051,b052,b053,b054,b055,b056,b057;// 48b
+    byte b060,b061,b062,b063,b064,b065,b066,b067;// 56b
+    byte b070,b071,b072,b073,b074,b075,b076,b077;// 64b
+    byte b100,b101,b102,b103,b104,b105,b106,b107;// 72b
+    byte b110,b111,b112,b113,b114,b115,b116,b117;// 80b
+    byte b120,b121,b122,b123,b124,b125,b126,b127;// 88b
+    byte b130,b131,b132,b133,b134,b135,b136,b137;// 96b
+    byte b140,b141,b142,b143,b144,b145,b146,b147;//104b
+    byte b150,b151,b152,b153,b154,b155,b156,b157;//112b
+    byte b160,b161,b162,b163,b164,b165,b166,b167;//120b
+    //byte b170,b171,b172,b173,b174,b175,b176,b177;//128b
 
     private static final long PROBE = getProbeOffset();
 
