2985331568a35c6844917470da46b2e35ea846e5
Nitsan <nwakart@gmail.com>
2021-02-25 11:24:09 +0200

    Rename test

--- jctools-core/src/test/java/org/jctools/maps/NBHMReplaceTest.java
+++ jctools-core/src/test/java/org/jctools/maps/NBHMReplaceTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class ReplaceTest {
+public class NBHMReplaceTest {
     @Test
     public void replaceOnEmptyMap() {
         assertEquals(null, new NonBlockingHashMap<String,String>().replace("k", "v"));
